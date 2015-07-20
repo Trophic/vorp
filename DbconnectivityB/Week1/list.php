4619cbccf5ea244a9c1d89b88d09b00ffa061d77
Rohan <rohanc94@gmail.com>
2015-07-20 19:22:22 +1000

    Added list 'solution'
    https://en.wikipedia.org/wiki/Category:Australian_actors

---
+++ DbconnectivityB/Week1/list.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $dudes = array("Michael Bodd", "John Fleeting", "Tosh Greenslade", "Kit Taylor", "Exodus Lale", "Lorna Westbrook", "Ozzie Devrish");
+        list($a, $b, $c, $d, $e, $f, $g) = $dudes;
+        echo "<p>Famous australian actors: <br />
+                1: $a,<br />
+                2: $b,<br />
+                3: $c,<br />
+                4: $d,<br />
+                5: $e,<br />
+                6: $f,<br />
+                7: $g <br /></p>";
+        ?>
+    </body>
+</html>
