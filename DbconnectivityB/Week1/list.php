f369eae49ac22256ecc901698b79e59b5243fd85
Rohan <rohanc94@gmail.com>
2015-07-20 20:03:06 +1000

    updated list.php
    totes all janusz

--- DbconnectivityB/Week1/list.php
+++ DbconnectivityB/Week1/list.php
@@ -1,21 +1,15 @@
+ <?php $dudes = ["cate blanchett","Michael Bodd", "John Fleeting", "Tosh Greenslade", "Kit Taylor", "Exodus Lale", "Lorna Westbrook", "Ozzie Devrish"]; ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        $dudes = array("Michael Bodd", "John Fleeting", "Tosh Greenslade", "Kit Taylor", "Exodus Lale", "Lorna Westbrook", "Ozzie Devrish");
-        list($a, $b, $c, $d, $e, $f, $g) = $dudes;
-        echo "<p>Famous australian actors: <br />
-                1: $a,<br />
-                2: $b,<br />
-                3: $c,<br />
-                4: $d,<br />
-                5: $e,<br />
-                6: $f,<br />
-                7: $g <br /></p>";
-        ?>
-    </body>
-</html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title></title>
+        </head>
+        <body>
+            <ol>
+            <?php foreach ($dudes as $dude): ?>
+            <li><?= $dude ?></li>
+            <?php endforeach ?>
+            </ol>    
+        </body>
+    </html>
